6d31304765258a0bf3a1e1c02f28f90c77300c90
Liam <leliam@live.fr>
2021-01-05 13:21:36 +0100

    [ADD] Icones UI Kitten

--- App.js
+++ App.js
@@ -2,15 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import { default as theme } from './theme.json';
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider} from '@ui-kitten/components';
+import {ApplicationProvider, Button, Icon, IconRegistry} from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import Home from './src/components/Home';
 
+const FacebookIcon = (props) => (
+    <Icon name='facebook' {...props} />
+);
+
+export const LoginButton = () => (
+    <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
+);
+
 export default function App() {
-  return (
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-        <Home/>
-        <StatusBar style="auto" />
-    </ApplicationProvider>
-  );
+    return (
+        <>
+            <IconRegistry icons={EvaIconsPack} />
+            <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+                <Home/>
+                <LoginButton/>
+                <StatusBar style="auto" />
+            </ApplicationProvider>
+        </>
+    );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ export default Home;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     }
